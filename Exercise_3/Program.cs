c7cc170e40a11e2a3faf38c474de0c1e37611c61
Aleksey <a_zhuravlyov@bk.ru>
2022-08-17 23:15:51 +0300

    Finished Ex 3

--- Exercise_3/Program.cs
+++ Exercise_3/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+class Program
+{
+    static void Main (string[] args)
+    {
+        Console.WriteLine("Введите число N - ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (double i = 1; i <= n; i++)
+        {
+            double result = Math.Pow(i, 3);
+
+            Console.Write(i + "^3" + " = " + result + "| ");
+        }
+    }
+}
